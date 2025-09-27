16099e1d3e7f09d353a3402049b679c14022eb1d
anhmt <maituananh12061998@gmail.com>
2025-09-27 16:09:42 +0700

    update CorsConfigurationSource

--- src/main/java/com/spring/backend/configuration/CorsAppConfiguration.java
+++ src/main/java/com/spring/backend/configuration/CorsAppConfiguration.java
@@ -11,24 +11,15 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 public class CorsAppConfiguration {
 
-  private static final List<String> ALLOW_HEADERS =
-      Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept");
-
-  private static final List<String> ALLOW_METHODS =
-      Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS");
-
   private static final List<String> ALLOW_EXPORT_HEADERS =
       Arrays.asList("Authorization", "Content-Disposition");
-
-  private static final List<String> ALLOW_ORIGINS =
-      Arrays.asList("http://localhost:3000", "http://localhost:5173");
 
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(ALLOW_ORIGINS); // Specify allowed origins
-    configuration.setAllowedMethods(ALLOW_METHODS); // Allowed HTTP methods
-    configuration.setAllowedHeaders(ALLOW_HEADERS); // Allowed request headers
+    configuration.addAllowedOrigin("*"); // Specify allowed origins
+    configuration.addAllowedHeader("*"); // Allowed HTTP methods
+    configuration.addAllowedMethod("*"); // Allowed request headers
     configuration.setAllowCredentials(
         true); // Allow credentials (e.g., cookies, authorization headers)
     configuration.setExposedHeaders(ALLOW_EXPORT_HEADERS); // Headers to expose to the client
