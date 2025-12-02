3ce4dd8e4dc3e92e01857353ffff8d9e08d936f8
Anh Kiet <asrieldreemurr685@gmail.com>
2025-12-02 19:27:19 +0700

    feat/Search/category-by-name

--- src/main/java/com/spring/backend/service/CategoryServiceImpl.java
+++ src/main/java/com/spring/backend/service/CategoryServiceImpl.java
@@ -49,23 +49,6 @@ public class CategoryServiceImpl implements CategoryService {
         .toList();
   }
 
-  public List<CategoryResponseDto> searchByName(String name) {
-    Specification<CategoryEntity> spec = CategoryRepository.search(name);
-
-    List<CategoryEntity> categories = categoryRepository.findAll(spec);
-
-    return categories.stream()
-        .map(
-            category -> {
-              UserEntity creator = null;
-              if (category.getCreatedBy() != null) {
-                creator = userRepository.findById(category.getCreatedBy()).orElse(null);
-              }
-              return CategoryMapper.toCategoryDto(category, creator);
-            })
-        .toList();
-  }
-
   @Override
   public Page<CategoryResponseDto> searchByName(String name, int page, int size) {
 
